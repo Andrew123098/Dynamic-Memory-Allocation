0e17379d72f7e16957fa688ea0933dd5909e7f99
Andrew Brown <andrewb1230@gmail.com>
2019-08-27 18:57:19 -0700

    Fixed fgets and scanf problem!

--- main.c
+++ main.c
@@ -4,30 +4,23 @@
 
 int main()
 {
-   int MAX;
-   printf("Please enter the MAX number of characters in your string: ");
-   scanf("%d", &MAX);
+    int MAX;
+    printf("Please enter the MAX number of characters in your string: ");
+    scanf("%d", &MAX);
 
-   char *pstr = NULL;
-   char str[MAX];
+    char *str = NULL;
 
-    *pstr = &str;
-    /**
-    This line of code causes my program to crash and idk why.
-    **/
+    str = (char*)malloc(MAX * sizeof(char));
 
+    if (str != NULL)
+    {
+        printf("\nPlease enter your string: ");
+        scanf(" "); // this line is needed to fix the problem that occurs when using fgets after scanf which automatically has a dellimeter at the end.
+        fgets(str, MAX, stdin);
+        printf("\nYour inputted text: %s", str);
+    }
 
-   //printf("\nPlease enter your string:");
-   //fgets(*pstr, MAX, stdin);
-
-    //str = (char*)malloc(MAX * sizeof(char));
-
-
-
-
-
-
+    free(str); //release the memory
+    str = NULL; //double check that you released the memory
     return 0;
-
-
 }
